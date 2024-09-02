3b16094af7b547274a4b593216fe27451e5f0f59
josephblue26601 <josephuwa86@gmail.com>
2024-09-02 07:38:38 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
-import { Suspense } from "react"; // Import Suspense from React
+import { Suspense } from "react"; 
 
 const inter = Inter({ subsets: ["latin"] });
 
